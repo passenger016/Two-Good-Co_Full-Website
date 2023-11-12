4dd17f0f9b264c59e268e30d3167b3f2fd481d4e
Tarak Nath Paul <passenger.code016@gmail.com>
2023-11-12 15:05:08 +0530

    new font file added , Futura-Medium , interacting cursor added

--- main.js
+++ main.js
@@ -9,12 +9,28 @@ const cursor = document.querySelector(".cursor");
 window.addEventListener('mousemove', (e) => {
     const x = e.clientX - cursor.offsetWidth / 2;
     const y = e.clientY - cursor.offsetHeight / 2;
+    let interactable = e.target.closest(".interactable");
+    const interacting = interactable !== null; // boolean that returns true if interacting else false
+    console.log(`interacting: ${interacting}`);
 
-    animateTrailer(e);
+
+    if (interacting) {
+        const data = interactable.dataset.type;
+        if (data == "product") {
+            console.log("product");
+            cursor.textContent = "click";
+        }
+    }
+    else {
+        cursor.textContent = "";
+    }
+
+
+    animateTrailer(e, interacting);
 })
 
 
-let animateTrailer = (e) => {
+let animateTrailer = (e, interacting) => {
     const x = e.clientX - cursor.offsetWidth / 2;
     const y = e.clientY - cursor.offsetHeight / 2;
 
@@ -24,12 +40,14 @@ let animateTrailer = (e) => {
     // make the mouse trailer lag a bit behind the cursor
     // creating an keyframes object
     const keyframes = {
-        transform:`translate(${x}px , ${y}px)`
+        transform: `translate(${x}px , ${y}px) scale(${(interacting) ? 5 : 1})`
     }
 
-    cursor.animate(keyframes,{
+    cursor.animate(keyframes, {
         duration: 700, // duration in mili seconds
         fill: "forwards" // starts back from the last ending point
     });
+
+
 }
 
